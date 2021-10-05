db1b1417805c719ffd932d7c01c8e7f75d26b7af
minh18112000 <nguyenducminh_t63@hus.edu.vn>
2021-10-05 16:08:05 +0700

    Init todoViewModel in MainActivity

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.todoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.example.todoapp.database.TodoDatabase
 import com.example.todoapp.databinding.ActivityMainBinding
+import com.example.todoapp.repository.TodoRepository
+import com.example.todoapp.viewmodel.TodoViewModel
+import com.example.todoapp.viewmodel.TodoViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var todoViewModel: TodoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +23,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+    }
+
+    private fun setUpViewModel() {
+        val todoRepository = TodoRepository(TodoDatabase(this))
+        val todoViewModelFactory = TodoViewModelFactory(application, todoRepository)
+
+        todoViewModel = ViewModelProvider(
+            this,
+            todoViewModelFactory
+        ).get(TodoViewModel::class.java)
+
     }
 }
--- app/src/main/java/com/example/todoapp/database/TodoDatabase.kt
+++ app/src/main/java/com/example/todoapp/database/TodoDatabase.kt
@@ -6,16 +6,25 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.todoapp.model.Todo
 
-
+// declare the entities for the database and set the version number
 @Database(entities = [Todo::class], version = 1)
 abstract class TodoDatabase : RoomDatabase() {
 
     abstract fun getTodoDao(): TodoDao
 
+    // The companion object allows clients to access the methods for
+    // creating or getting the database without instantiating the class.
+    // Since the only purpose of this class is to provide a database,
+    // there is no reason to ever instantiate it.
     companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
         @Volatile
         private var instance: TodoDatabase? = null
         private val LOCK = Any()
+
+        // if the instance is not null, then return it,
+        // if it is, then create the database
 
         operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
             instance ?: createDatabase(context).also { instance = it }
@@ -24,7 +33,7 @@ abstract class TodoDatabase : RoomDatabase() {
         private fun createDatabase(context: Context) = Room.databaseBuilder(
             context.applicationContext,
             TodoDatabase::class.java,
-            "note_db"
+            "todo_db"
         ).fallbackToDestructiveMigration()
             .build()
 
