22cfbe80dcf6655ebae299defad9d3df1c4a2401
minh18112000 <nguyenducminh_t63@hus.edu.vn>
2021-10-05 11:42:46 +0700

    Config todo model and RoomDatabase

---
+++ app/src/main/java/com/example/todoapp/database/TodoDao.kt
@@ -0,0 +1,27 @@
+package com.example.todoapp.database
+
+import androidx.room.*
+import com.example.todoapp.model.Todo
+
+/*
+ A data access object (DAO) to map Kotlin functions to SQL queries
+ When you use a Room database, you query the database by defining and
+ calling Kotlin functions in your code. These Kotlin functions map to
+ SQL queries. You define those mappings in a DAO using annotations, and
+ Room creates the necessary code.
+*/
+@Dao
+interface TodoDao {
+
+    // it triggers whenever you try to insert an object that has the same @PrimaryKey
+    // as an object of the same type that is already saved on the database. REPLACE
+    // will overwrite the item in the database with the new item.
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addTodo(todo: Todo)
+
+    @Update
+    suspend fun updateTodo(todo: Todo)
+
+    @Delete
+    suspend fun deleteTodo(todo: Todo)
+}
---
+++ app/src/main/java/com/example/todoapp/model/Todo.kt
@@ -0,0 +1,12 @@
+package com.example.todoapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todos")
+data class Todo(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val todoTitle: String,
+    val importantLevel: String
+)
