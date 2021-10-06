c69511d4f51d61b2189afc5620262dd9516418e3
minh18112000 <nguyenducminh_t63@hus.edu.vn>
2021-10-06 10:58:46 +0700

    View updated todo on screen

--- app/src/main/java/com/example/todoapp/fragment/UpdateTodoFragment.kt
+++ app/src/main/java/com/example/todoapp/fragment/UpdateTodoFragment.kt
@@ -3,8 +3,11 @@ package com.example.todoapp.fragment
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import com.example.todoapp.MainActivity
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentUpdateTodoBinding
+import com.example.todoapp.model.Todo
+import com.example.todoapp.viewmodel.TodoViewModel
 
 class UpdateTodoFragment : Fragment(R.layout.fragment_update_todo) {
 
@@ -12,17 +15,62 @@ class UpdateTodoFragment : Fragment(R.layout.fragment_update_todo) {
     private val binding
         get() = _binding!!
 
+    private lateinit var currentTodo: Todo
+    private lateinit var todoViewModel: TodoViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
 
+        // Inflate the layout for this fragment
         _binding = FragmentUpdateTodoBinding.inflate(
             inflater,
             container,
             false
         )
+
+        // get the viewModel
+        todoViewModel = (activity as MainActivity).todoViewModel
+
+        // extract the argument from the bundle]
+        val args = UpdateTodoFragmentArgs.fromBundle(requireArguments())
+        currentTodo = args.todo!!
+
+        // set title and body of item is updated
+        binding.etTodoTitle.setText(currentTodo.todoTitle)
+
+        var importantLevel: Int = currentTodo.importantLevel
+
+        if (importantLevel == 1) {
+            binding.imgImportantLevelOne.setImageResource(R.drawable.ic_done)
+        } else if (importantLevel == 2) {
+            binding.imgImportantLevelTwo.setImageResource(R.drawable.ic_done)
+        } else {
+            binding.imgImportantLevelThree.setImageResource(R.drawable.ic_done)
+        }
+
+
+        binding.imgImportantLevelOne.setOnClickListener {
+            binding.imgImportantLevelOne.setImageResource(R.drawable.ic_done)
+            binding.imgImportantLevelTwo.setImageResource(0)
+            binding.imgImportantLevelThree.setImageResource(0)
+            importantLevel = 1
+        }
+
+        binding.imgImportantLevelTwo.setOnClickListener {
+            binding.imgImportantLevelTwo.setImageResource(R.drawable.ic_done)
+            binding.imgImportantLevelOne.setImageResource(0)
+            binding.imgImportantLevelThree.setImageResource(0)
+            importantLevel = 2
+        }
+
+        binding.imgImportantLevelThree.setOnClickListener {
+            binding.imgImportantLevelThree.setImageResource(R.drawable.ic_done)
+            binding.imgImportantLevelTwo.setImageResource(0)
+            binding.imgImportantLevelOne.setImageResource(0)
+            importantLevel = 3
+        }
 
         return binding.root
     }
